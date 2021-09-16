6744d153742b1b1e5e859983782e333e75396226
Denisse23 <denissesuazo@hotmail.com>
2021-09-16 02:22:18 -0600

    Removing header on AuthNavigator and adding returnToLogin button in signUp Screen

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 
 const { Navigator, Screen } = createStackNavigator();
 
-export const AuthNavigator = () => (
-  <Navigator>
-    <Screen name='LoginScreen' component={LoginScreen}/>
-    <Screen name='SignUpScreen' component={SignUpScreen}/>
-  </Navigator>
-);
+export const AuthNavigator = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white'
+        }
+      }}
+    >
+      <Screen name='LoginScreen' component={LoginScreen}/>
+      <Screen name='SignUpScreen' component={SignUpScreen}/>
+    </Navigator>
+  )
+}
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import {StyleSheet} from 'react-native';
+import { scale } from 'react-native-size-matters';
+import { StackScreenProps } from '@react-navigation/stack'
 
 import CustomInput from '../components/CustomInput';
 import FormContainer from '../components/FormContainer';
@@ -13,10 +16,15 @@ import {
     EMAIL_LABEL, 
     EMAIL_PLACEHOLDER,
     PASSWORD_LABEL,
-    PASSWORD_PLACEHOLDER
+    PASSWORD_PLACEHOLDER,
+    RETURN_TO_LOGIN
 } from '../resources/translations/translationKeyConstants';
+import CenterContentContainer from '../components/CenterContentContainer';
+import CustomButton from '../components/CustomButton';
 
-const SignUpScreen = () => {
+interface Props extends StackScreenProps<any, any> { }
+
+const SignUpScreen = ({ navigation }: Props) => {
 
     const { email, password, firstName, lastName, onChange } = useForm({
         email: '',
@@ -27,42 +35,61 @@ const SignUpScreen = () => {
 
     return (
         <BaseScreen>
-            <FormContainer>
-                <CustomInput 
-                    label={FIRST_NAME_LABEL} 
-                    placeholder={FIRST_NAME_PLACEHOLDER}
-                    value={firstName}
-                    autoCorrect={false}
-                    autoCapitalize='sentences'
-                    onChangeText={(value) => onChange(value, 'firstName')}/>
-                <CustomInput 
-                    label={LAST_NAME_LABEL} 
-                    placeholder={LAST_NAME_PLACEHOLDER}
-                    value={lastName}
-                    autoCorrect={false}
-                    autoCapitalize="sentences"
-                    onChangeText={(value) => onChange(value, 'lastName')}/>
-                <CustomInput 
-                    label={EMAIL_LABEL} 
-                    placeholder={EMAIL_PLACEHOLDER}
-                    value={email}
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    keyboardType="email-address"
-                    onChangeText={(value) => onChange(value, 'email')}/>
-                <CustomInput    
-                    label={PASSWORD_LABEL}
-                    placeholder={PASSWORD_PLACEHOLDER}
-                    value={password}
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    secureTextEntry
-                    onChangeText={(value) => onChange(value, 'password')}
+            <CenterContentContainer>
+                <FormContainer>
+                    <CustomInput 
+                        label={FIRST_NAME_LABEL} 
+                        placeholder={FIRST_NAME_PLACEHOLDER}
+                        value={firstName}
+                        autoCorrect={false}
+                        autoCapitalize='sentences'
+                        onChangeText={(value) => onChange(value, 'firstName')}/>
+                    <CustomInput 
+                        label={LAST_NAME_LABEL} 
+                        placeholder={LAST_NAME_PLACEHOLDER}
+                        value={lastName}
+                        autoCorrect={false}
+                        autoCapitalize="sentences"
+                        onChangeText={(value) => onChange(value, 'lastName')}/>
+                    <CustomInput 
+                        label={EMAIL_LABEL} 
+                        placeholder={EMAIL_PLACEHOLDER}
+                        value={email}
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        keyboardType="email-address"
+                        onChangeText={(value) => onChange(value, 'email')}/>
+                    <CustomInput    
+                        label={PASSWORD_LABEL}
+                        placeholder={PASSWORD_PLACEHOLDER}
+                        value={password}
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        secureTextEntry
+                        onChangeText={(value) => onChange(value, 'password')}
+                    />
+                </FormContainer>
+                <CustomButton
+                    size='medium'
+                    appearance='ghost'
+                    status='primary'
+                    activeOpacity={0.6}
+                    buttonText={RETURN_TO_LOGIN}
+                    style={ styles.returnToLoginButton}
+                    onPress={ () => navigation.replace('LoginScreen') }
                 />
-            </FormContainer>
+            </CenterContentContainer>
         </BaseScreen>
     );
 };
+
+const styles = StyleSheet.create({
+    returnToLoginButton: {
+        alignSelf: 'flex-end',
+        marginRight: scale(40)
+    },
+    
+});
 
 
 export default SignUpScreen;
